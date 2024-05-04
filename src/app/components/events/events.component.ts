7a26d3d96eaf235d915383210531c5650ad3e6e4
SpencerBerger <143113025+SpencerBerger@users.noreply.github.com>
2024-05-04 16:19:09 -0500

    Events page update

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Event } from '../../models/event.model';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  events: Event[] = [];
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getEvents().subscribe(events => {
+      this.events = events;
+    }, error => {
+      console.error('Error fetching events:', error);
+    });
+  }
+
+  getImageUrl(event: Event): string {
+    if (event && event._id) {
+      return this.dataService.getImageUrl('event', event._id);
+    } else {
+      // Return a default image URL or handle the case where _id is undefined
+      return ''; // Default URL or appropriate handling
+    }
+  }
 }
